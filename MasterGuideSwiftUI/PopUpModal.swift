2a52ca8d39494a786d67dc972f2b938cc2cdc87a
Marco Alonso <marcoalonsorota@hotmail.com>
2024-10-03 10:04:42 -0600

    Searchbar added to filter example views

---
+++ MasterGuideSwiftUI/PopUpModal.swift
@@ -0,0 +1,18 @@
+//
+//  PopUpModal.swift
+//  MasterGuideSwiftUI
+//
+//  Created by Marco Alonso on 03/10/24.
+//
+
+import SwiftUI
+
+struct PopUpModal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PopUpModal()
+}
