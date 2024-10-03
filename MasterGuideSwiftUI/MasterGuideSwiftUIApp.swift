8e6b24c35d41770612107b23895f8264db6b2e3b
Marco Alonso <marcoalonsorota@hotmail.com>
2024-10-03 09:20:35 -0600

    Initial Commit

---
+++ MasterGuideSwiftUI/MasterGuideSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasterGuideSwiftUIApp.swift
+//  MasterGuideSwiftUI
+//
+//  Created by Marco Alonso on 03/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MasterGuideSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
